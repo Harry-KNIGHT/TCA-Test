926efa9836555911b771e813e6b6eb9811567700
Harry-KNIGHT <eliott.knight@icloud.com>
2022-12-18 12:07:51 +0100

    Initial Commit

---
+++ TCA-Test/TCA_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TestApp.swift
+//  TCA-Test
+//
+//  Created by Elliot Knight on 18/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

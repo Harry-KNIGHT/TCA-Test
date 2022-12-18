473c60560abb2f16effda55902f4e52600f24976
Harry-KNIGHT <eliott.knight@icloud.com>
2022-12-18 12:59:22 +0100

    Use TCA for increase or decrease counter.

--- TCA-Test/ContentView.swift
+++ TCA-Test/ContentView.swift
@@ -6,21 +6,63 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct State: Equatable {
+	var counter = 0
+}
+
+enum Action: Equatable {
+	case increaseCounter, deacraseCounter
+}
+
+struct Environment {
+	// Future dependencies
+}
+
+let reducer = Reducer<State, Action, Environment> { state, action, environment in
+	switch action {
+	case .increaseCounter:
+		state.counter += 1
+		return Effect.none
+	case .deacraseCounter:
+		state.counter -= 1
+		return Effect.none
+	}
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	let store: Store<State, Action>
+
+	var body: some View {
+		WithViewStore(self.store) { viewStore in
+
+			HStack {
+				Button {
+					viewStore.send(.deacraseCounter)
+				} label: {
+					Image(systemName: "minus")
+				}
+
+				Text(viewStore.state.counter.description)
+
+				Button {
+					viewStore.send(.increaseCounter)
+				} label: {
+					Image(systemName: "plus")
+				}
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView(
+			store: Store(
+				initialState: State(),
+				reducer: reducer,
+				environment: Environment())
+		)
+	}
 }
